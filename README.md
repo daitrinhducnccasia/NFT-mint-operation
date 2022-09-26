990d684f0fd3eaaa4206de2aa0ad95292a2bbd6a
suricata3838 <summer970827@gmail.com>
2022-09-27 00:44:02 +0900

    impl file export

---
+++ README.md
@@ -0,0 +1,6 @@
+## Binaceアカウント上の設定の注意点
+
+- 出金が可能になっているAPIのAPIKey, secretを取得し、`.env`に記載する。
+
+- 出金ホワイトリストがオフになっていること
+  - [Securityページ](https://www.binance.com/ja/my/security)から出金ホワイトリストをオフにする。
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Spot }= require('@binance/connector')
+const fs = require('fs')
 require('dotenv').config()
 
 const secret = process.env.SECRET
@@ -14,33 +15,128 @@ const accountStatus = async () => {
     }
 }
 
-const userAsset = async () => {
+const userAsset = async (asset) => {
+    console.log("userAsset:", asset);
+    const options = {asset}
     try {
-        const res = await client.userAsset()
+        const res = await client.userAsset(options)
         client.logger.log(res.data)
     } catch(e) {
         client.logger.error(e)
     }
 }
 
-const withdrawHistory = async () => {
+const withdrawHistory = async(_asset, _counter) => {
     const opt = {
-        coin: 'USDT',
+        coin: _asset,
         status: 6,
-        startTime: new Date('2022-07-01').getTime()
+        withdrawOrderId: _counter.toString()// withdrawCounter.toString()
     }
     try {
         const res = await client.withdrawHistory(opt)
         client.logger.log(res.data)
-    } catch(e) {
-        client.logger.error(e)
+        return res.data
+    } catch(err) {
+        client.logger.error(err.response.headers) // full response header
+        client.logger.error(err.response.status) // 400
+        client.logger.error(err.response.data)
     }
 }
 
+// 10.0 is the minimal. 0.29 USDT network fee
+const withdarwAmoutUSDT = 10.29;
+// withdraw USDT from BSC network.
+const withdrawUSDT = async(addr, _counter) => {
+    const options = {
+        withdrawOrderId: _counter.toString(),
+        network: 'BSC',
+        // walletType: 0 //default: 0-Spot 
+    }
+    try {
+        const res = await client.withdraw(
+            'USDT',
+            addr,
+            withdarwAmoutUSDT,
+            options)
+        // client.logger.log(res.data)// {id: 0418060f3f4e416ca7686acdc4e33955}
+        _counter ++
+        return res.data
+    } catch(err) {
+        client.logger.error(err.response.headers) // full response header
+        client.logger.error(err.response.status) // 400
+        client.logger.error(err.response.data)// error code and msg
+    }
+}
+
+// 0.0098 is minimal. 0.00096 ETH is network fee.
+const withdarwAmoutETH = 0.01
+// withdraw ETH from ETH network.
+const withdrawETH = async (addr, _counter) => {
+    const options = {
+        withdrawOrderId: _counter.toString(),
+        network: 'ETH',
+        // walletType: 0 //default: 0-Spot 
+    }
+    try {
+        const res = await client.withdraw(
+            'ETH',
+            addr,
+            withdarwAmoutETH,
+            options)
+        // client.logger.log(res.data)// e.g. {id: 0418060f3f4e416ca7686acdc4e33955}
+        _counter ++
+        return res.data
+    } catch(err) {
+        client.logger.error(err.response.headers) // full response header
+        client.logger.error(err.response.status) // 400
+        client.logger.error(err.response.data)// error code and msg
+    }
+}
+
+
 const main = async() => {
-    // await accountStatus()
-    // await userAsset()
-    await withdrawHistory()
+    /// Configuration for withdrawAsset
+    const asset = 'USDT'
+    // Note: Counterは毎回ユニークに。
+    let withdrawCounter = 100
+    const destinationAddressList = ['0xa6829D7C72618A54b77F5D63158d7333dD4dD293']
+    let withdrawIds = ['85c1719579c3491993c8ea4e97ce6d6b', '0418060f3f4e416ca7686acdc4e33955']
+
+    try {
+        await userAsset(asset)
+
+        // await Promise.all(
+        //     destinationAddressList.map(async (addr) => {
+        //         /// Comment Out: Execution to withdraw
+        //         switch(asset) {
+        //             case 'ETH':
+        //                 // const id = await withdrawUSDT(addr, withdrawCounter)
+        //                 withdrawIds.push(id)
+        //                 break;
+        //             case 'USDT':
+        //                 const id = await withdrawUSDT(addr, withdrawCounter)
+        //                 withdrawIds.push(id)
+        //         }
+        //     }))
+
+        let logArray = new Array();
+        await Promise.all(
+            withdrawIds.map(async (id, idx) => {
+                const logPerId = await withdrawHistory(asset, idx)
+                logArray[idx] = logPerId
+            }))
+        
+        const dateTime = new Date().toISOString()
+        const fileName = dateTime + "-" + asset + "-output.log"
+        console.log(fileName)
+        var file = fs.createWriteStream(fileName)
+        file.on('error', (err) => { console.error(err); })
+        logArray.forEach(
+            (line) => { file.write(JSON.stringify(line) + '\n'); })
+        file.end()
+    }catch(e){
+        console.error(e)
+    }
 }
 
 main()
